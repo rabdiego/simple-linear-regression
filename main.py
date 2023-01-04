ca7e3993e80ed5aece20ab203d84264f76cc6499
nikrs14 <diegorabelodesa@gmail.com>
2023-01-04 19:22:34 -0300

    Training and predicting done

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # Importing the dataset
 data = pd.read_csv('Salary_Data.csv')
 X = data.iloc[:, :-1].values
-y = data[:, -1].values
+y = data.iloc[:, -1].values
 
 # Splitting into train set and test set
 X_train, X_test, y_train, y_set = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Training model
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the test set
+y_pred = regressor.predict(X_test)
